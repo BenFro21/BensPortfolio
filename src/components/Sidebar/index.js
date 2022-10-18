99e955d09233f97f8629fa81f4357862dfbbf730
benfro21 <broad0601@gmail.com>
2022-10-18 15:36:18 -0400

    Started mobile responsiveness

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.jpg'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome, faSuitcase, faUser} from '@fortawesome/free-solid-svg-icons'
+import {faBars, faEnvelope, faHome, faSuitcase, faUser} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
@@ -61,6 +61,13 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+        <FontAwesomeIcon
+       icon={faBars}
+       color="#ffd700"
+       size='3x'
+       className='hamburger-icon' 
+        />
+
     </div>
 )
 
